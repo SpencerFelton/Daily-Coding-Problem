24df0a54aa984cec7b15c30da1b292ea12a89514
Spencer Felton <SpencerEFelton@gmail.com>
2020-07-10 17:45:43 +0100

    Create ListSumProblem.py

---
+++ 10-07-2020/ListSumProblem.py
@@ -0,0 +1,11 @@
+def isSolution(list, value):
+    possible_solutions = []
+    for values in list:
+        required_solution = value - values
+        if values in possible_solutions:
+            return True
+        possible_solutions.append(required_solution)
+    return False
+
+print(isSolution([1,2,3], 6)) #false
+print(isSolution([10,15,5], 20)) #true
