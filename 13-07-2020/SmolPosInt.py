cde82af8c20d38a07d1196f475450f22155f5c9a
Spencer Felton <SpencerEFelton@gmail.com>
2020-07-14 21:32:03 +0100

    Function to find the next smallest non-appearing integer in a list

---
+++ 13-07-2020/SmolPosInt.py
@@ -0,0 +1,24 @@
+def smolInt(listToSort):
+    noNegatives = [nums for nums in listToSort if nums >= 0]
+    noNegatives.sort()
+    noDupes = set(noNegatives)
+    noDupesList = list(noDupes)
+
+    if noDupesList == []: #empty list, smallest int therefor 1
+        return 1
+
+    smallestVal = noDupesList[0] #list is sorted, smallest value is 1st element
+    if smallestVal > 1: # if 1st element is more than 1, smallest possible missing int is 1
+        return 1
+
+    for x in range(1, len(noDupesList)): #check between 2nd and last element
+        if x == len(noDupesList)-1: #final element
+            if noDupesList[x] > smallestVal + 1: # more than 1 above current smallest value in chain
+                return smallestVal + 1 # so next smallest int is the smallest value + 1
+            else:
+                return noDupesList[x] + 1 #otherwise all ints in chain 1->2->3 etc, so next smallest is final element + 1
+        if noDupesList[x] > smallestVal + 1:
+            return smallestVal + 1
+        smallestVal = noDupesList[x] # current element is in the chain x-1, x, x+1 etc, so it's the new smallest val
+
+print(smolInt([1,4,5]))
