288718ae50bcbc1e6f582550bfda5440675095a1
Spencer Felton <SpencerEFelton@gmail.com>
2020-10-20 23:27:25 +0100

    Create substring.py

---
+++ 20-10-2020/substring.py
@@ -0,0 +1,14 @@
+def sub_string(substring, string):
+    sub = ""
+    for sub_letters in substring:
+        if(sub_letters in string and sub_letters not in sub):
+                sub += sub_letters
+    print(sub)
+    return sub
+
+string1 = "aaaaaaaaaaaaa"
+string1 = list(string1)
+substring = "eci"
+substring = list(substring)
+
+sub_string(substring, string1)
